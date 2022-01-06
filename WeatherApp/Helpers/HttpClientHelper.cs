d486ee1bc4728f7c39d44b37841274b2ddff1940
Paul Johnson <paul_johnson1@next.co.uk>
2022-01-06 14:48:43 +0000

    new httpclienthelper class

---
+++ WeatherApp/Helpers/HttpClientHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WeatherApp.Helpers
+{
+    public class HttpClientHelper
+    {
+        public HttpClient SetupClient(int timeOut = -1, bool isMinutes = true, bool acceptMediaType = false, string mediaType = "")
+        {
+            var client = new HttpClient();
+            if (timeOut != -1)
+            {
+                if (isMinutes)
+                    client.Timeout = TimeSpan.FromMinutes(timeOut);
+                else
+                    client.Timeout = TimeSpan.FromSeconds(timeOut);
+            }
+
+            if (acceptMediaType && !string.IsNullOrEmpty(mediaType))
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(mediaType));
+            }
+               
+            return client;
+        }
+    }
+}
