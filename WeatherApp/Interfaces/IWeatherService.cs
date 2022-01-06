3b4eef17fc3caed3c4f0cc8faa7845aff5510de7
Paul Johnson <paul_johnson1@next.co.uk>
2022-01-06 14:49:19 +0000

    renamed

---
+++ WeatherApp/Interfaces/IWeatherService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Interfaces
+{
+    public interface IWeatherService
+    {
+        Task<WeatherData> GetWeatherForLocation(double lng, double lat);
+        Task<WeatherData> GetWeatherForCity(string city, string country, string state ="");
+    }
+}
