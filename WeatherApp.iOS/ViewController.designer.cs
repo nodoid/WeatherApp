3a649168838a1167eed46d0e55b3ff8f7c40f806
Paul Johnson <paul_johnson1@next.co.uk>
2022-01-07 02:20:50 +0000

    Update ViewController.designer.cs

--- WeatherApp.iOS/ViewController.designer.cs
+++ WeatherApp.iOS/ViewController.designer.cs
@@ -1,210 +1,209 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace WeatherApp.iOS
 {
-	[Register("ViewController")]
+	[Register ("ViewController")]
 	partial class ViewController
 	{
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UIButton btnOK { get; set; }
+		UIKit.UIButton btnGetWeather { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UIButton btnGetWeather { get; set; }
+		UIKit.UIButton btnOK { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UIButton btnResetData { get; set; }
+		UIKit.UIButton btnReset { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UIButton btnResetLoc { get; set; }
+		UIKit.UIButton btnResetLocation { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UITextView txtCity { get; set; }
+		UIKit.UILabel lblDegrees { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UITextView txtState { get; set; }
+		UIKit.UILabel lblGeolocation { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UITextView txtCountry { get; set; }
+		UIKit.UILabel lblHumid { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UISwitch switchCurrentLoc { get; set; }
+		UIKit.UILabel lblPlace { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UISwitch switchSave { get; set; }
+		UIKit.UILabel lblPressure { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UIActivityIndicatorView spinProgress { get; set; }
+		UIKit.UILabel lblSky { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UIView viewResults { get; set; }
+		UIKit.UILabel lblSpeed { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblSky { get; set; }
+		UIKit.UILabel lblSunset { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblTemp { get; set; }
+		UIKit.UILabel lblSunUp { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblTempMax { get; set; }
+		UIKit.UILabel lblTemp { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblTempMin { get; set; }
+		UIKit.UILabel lblTempMax { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblPressure { get; set; }
+		UIKit.UILabel lblTempMin { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblHumid { get; set; }
+		UIKit.UILabel lblWind { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblSunrise { get; set; }
+		UIKit.UIActivityIndicatorView spinProgress { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblSunset { get; set; }
+		UIKit.UISwitch switchSaveLocation { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblSpeed { get; set; }
+		UIKit.UISwitch switchUseLocation { get; set; }
 
 		[Outlet]
-		[GeneratedCode("iOS Designer", "1.0")]
-		UILabel lblDegrees { get; set; }
+		UIKit.UITextField txtCity { get; set; }
 
-		void ReleaseDesignerOutlets()
+		[Outlet]
+		UIKit.UITextField txtCountry { get; set; }
+
+		[Outlet]
+		UIKit.UITextField txtState { get; set; }
+
+		[Outlet]
+		UIKit.UIView viewResults { get; set; }
+		
+		void ReleaseDesignerOutlets ()
 		{
-			if (btnOK != null)
-			{
-				btnOK.Dispose();
-				btnOK = null;
-			}
-			if (btnGetWeather != null)
-			{
-				btnGetWeather.Dispose();
+			if (btnGetWeather != null) {
+				btnGetWeather.Dispose ();
 				btnGetWeather = null;
 			}
-			if (btnResetData != null)
-			{
-				btnResetData.Dispose();
-				btnResetData = null;
-			}
-			if (btnResetLoc != null)
-			{
-				btnResetLoc.Dispose();
-				btnResetLoc = null;
-			}
-			if (txtCity != null)
-			{
-				txtCity.Dispose();
-				txtCity = null;
-			}
-			if (txtCountry != null)
-			{
-				txtCountry.Dispose();
-				txtCountry = null;
-			}
-			if (txtState != null)
-			{
-				txtState.Dispose();
-				txtState = null;
-			}
-			if (switchCurrentLoc != null)
-			{
-				switchCurrentLoc.Dispose();
-				switchCurrentLoc = null;
-			}
-			if (switchSave != null)
-			{
-				switchSave.Dispose();
-				switchSave = null;
-			}
-			if (spinProgress != null)
-			{
-				spinProgress.Dispose();
-				spinProgress = null;
-			}
-			if (viewResults != null)
-			{
-				viewResults.Dispose();
-				viewResults = null;
+
+			if (btnOK != null) {
+				btnOK.Dispose ();
+				btnOK = null;
 			}
 
-			if (lblSky != null)
-			{
-				lblSky.Dispose();
-				lblSky = null;
+			if (btnReset != null) {
+				btnReset.Dispose ();
+				btnReset = null;
 			}
-			if (lblTemp != null)
-			{
-				lblTemp.Dispose();
-				lblTemp = null;
+
+			if (btnResetLocation != null) {
+				btnResetLocation.Dispose ();
+				btnResetLocation = null;
 			}
-			if (lblTempMin != null)
-			{
-				lblTempMin.Dispose();
-				lblTempMin = null;
-			}
-			if (lblTempMax != null)
-			{
-				lblTempMax.Dispose();
-				lblTempMax = null;
-			}
-			if (lblPressure != null)
-			{
-				lblPressure.Dispose();
-				lblPressure = null;
-			}
-			if (lblHumid != null)
-			{
-				lblHumid.Dispose();
-				lblHumid = null;
-			}
-			if (lblSunrise != null)
-			{
-				lblSunrise.Dispose();
-				lblSunrise = null;
-			}
-			if (lblSunset != null)
-			{
-				lblSunset.Dispose();
-				lblSunset = null;
-			}
-			if (lblDegrees != null)
-			{
-				lblDegrees.Dispose();
+
+			if (lblDegrees != null) {
+				lblDegrees.Dispose ();
 				lblDegrees = null;
 			}
-			if (lblSpeed != null)
-			{
-				lblSpeed.Dispose();
+
+			if (lblGeolocation != null) {
+				lblGeolocation.Dispose ();
+				lblGeolocation = null;
+			}
+
+			if (lblHumid != null) {
+				lblHumid.Dispose ();
+				lblHumid = null;
+			}
+
+			if (lblPlace != null) {
+				lblPlace.Dispose ();
+				lblPlace = null;
+			}
+
+			if (lblPressure != null) {
+				lblPressure.Dispose ();
+				lblPressure = null;
+			}
+
+			if (lblSky != null) {
+				lblSky.Dispose ();
+				lblSky = null;
+			}
+
+			if (lblSpeed != null) {
+				lblSpeed.Dispose ();
 				lblSpeed = null;
+			}
+
+			if (lblSunset != null) {
+				lblSunset.Dispose ();
+				lblSunset = null;
+			}
+
+			if (lblSunUp != null) {
+				lblSunUp.Dispose ();
+				lblSunUp = null;
+			}
+
+			if (lblTemp != null) {
+				lblTemp.Dispose ();
+				lblTemp = null;
+			}
+
+			if (lblTempMax != null) {
+				lblTempMax.Dispose ();
+				lblTempMax = null;
+			}
+
+			if (lblTempMin != null) {
+				lblTempMin.Dispose ();
+				lblTempMin = null;
+			}
+
+			if (lblWind != null) {
+				lblWind.Dispose ();
+				lblWind = null;
+			}
+
+			if (spinProgress != null) {
+				spinProgress.Dispose ();
+				spinProgress = null;
+			}
+
+			if (switchSaveLocation != null) {
+				switchSaveLocation.Dispose ();
+				switchSaveLocation = null;
+			}
+
+			if (switchUseLocation != null) {
+				switchUseLocation.Dispose ();
+				switchUseLocation = null;
+			}
+
+			if (txtCity != null) {
+				txtCity.Dispose ();
+				txtCity = null;
+			}
+
+			if (txtCountry != null) {
+				txtCountry.Dispose ();
+				txtCountry = null;
+			}
+
+			if (txtState != null) {
+				txtState.Dispose ();
+				txtState = null;
+			}
+
+			if (viewResults != null) {
+				viewResults.Dispose ();
+				viewResults = null;
 			}
 		}
 	}
