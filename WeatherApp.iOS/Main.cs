d8df121a45105430945323ec4151aa1377206785
Paul Johnson <paul_johnson1@next.co.uk>
2022-01-07 02:20:08 +0000

    add try / catch

--- WeatherApp.iOS/Main.cs
+++ WeatherApp.iOS/Main.cs
@@ -1,4 +1,5 @@
-﻿using UIKit;
+﻿using System;
+using UIKit;
 
 namespace WeatherApp.iOS
 {
@@ -7,9 +8,14 @@ namespace WeatherApp.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, typeof(AppDelegate));
+            try
+            {
+                UIApplication.Main(args, null, typeof(AppDelegate));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error : {ex.Message} -- {ex.InnerException?.Message}");
+            }
         }
     }
 }
