86fadfda4a04aa95cd89de2a6e2535ae27859f9a
Paul Johnson <paul_johnson1@next.co.uk>
2022-01-11 16:00:19 +0000

    DBConnection is nullable

--- WeatherApp/Constants/Constants.cs
+++ WeatherApp/Constants/Constants.cs
@@ -6,6 +6,6 @@ namespace WeatherApp.Constants
     {
         public static string APIKey { get; private set; } = "8701bd54bcc76072ef55857b9c892a3f";
         public static string BaseUri { get; private set; } = "https://api.openweathermap.org/data/2.5/weather?";
-        public static SQLiteConnection DBConnection { get; set; }
+        public static SQLiteConnection? DBConnection { get; set; }
     }
 }
