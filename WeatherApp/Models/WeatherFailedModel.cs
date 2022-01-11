924a89ff9235d621e487034ba728798f2e8ca67b
Paul Johnson <paul_johnson1@next.co.uk>
2022-01-11 16:00:29 +0000

    new model

---
+++ WeatherApp/Models/WeatherFailedModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WeatherApp.Models
+{
+    public class WeatherFailedModel
+    {
+#nullable enable
+        [JsonProperty("message")]
+        public string? Message { get; set; }
+        [JsonProperty("list")]
+        public List<object>? List { get; set; }
+#nullable disable
+        [JsonProperty("cod")]
+        public int Cod { get; set; }
+        [JsonProperty("count")]
+        public int Count { get; set; }
+    }
+}
