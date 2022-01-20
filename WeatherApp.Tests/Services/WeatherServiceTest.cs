732fbb421cb8f8dac2635b09b8161a454f471593
Paul Johnson <paul_johnson1@next.co.uk>
2022-01-20 14:25:21 +0000

    use mock data class

--- WeatherApp.Tests/Services/WeatherServiceTest.cs
+++ WeatherApp.Tests/Services/WeatherServiceTest.cs
@@ -10,6 +10,7 @@ using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using WeatherApp.Models;
+using WeatherApp.Tests.Helpers;
 using WeatherApp.Tests.Interfaces;
 using WeatherApp.Tests.Models;
 
@@ -28,56 +29,6 @@ namespace WeatherApp.Tests.Services
             Connection.NetworkConnected().Returns(true);
             httpMessageHandler = Substitute.For<HttpMessageHandler>();
         }
-
-        [SetUp]
-        public WeatherData Setup_WeatherData()
-        {
-            var data = @"{
-  ""coord"": {
-    ""lon"": -122.08,
-    ""lat"": 37.39
-  },
-  ""weather"": [
-    {
-      ""id"": 800,
-      ""main"": ""Clear"",
-      ""description"": ""clear sky"",
-      ""icon"": ""01d""
-    }
-  ],
-  ""base"": ""stations"",
-  ""main"": {
-    ""temp"": 282.55,
-    ""feels_like"": 281.86,
-    ""temp_min"": 280.37,
-    ""temp_max"": 284.26,
-    ""pressure"": 1023,
-    ""humidity"": 100
-  },
-  ""visibility"": 16093,
-  ""wind"": {
-    ""speed"": 1.5,
-    ""deg"": 350
-  },
-  ""clouds"": {
-    ""all"": 1
-  },
-  ""dt"": 1560350645,
-  ""sys"": {
-    ""type"": 1,
-    ""id"": 5122,
-    ""message"": 0.0139,
-    ""country"": ""US"",
-    ""sunrise"": 1560343627,
-    ""sunset"": 1560396563
-  },
-  ""timezone"": -25200,
-  ""id"": 420006353,
-  ""name"": ""Mountain View"",
-  ""cod"": 200
-  }";
-            return JsonConvert.DeserializeObject<WeatherData>(data);
-        }
         
 
         [Test]
@@ -85,7 +36,7 @@ namespace WeatherApp.Tests.Services
         {
             var api = "q=";
             var pars = string.IsNullOrEmpty(state) ? $"{city},{country}" : $"{city},{state},{country}";
-            var data = Setup_WeatherData();
+            var data = new MockWeatherDataSetup().Setup_WeatherData();
             Assert.IsNotNull(data);
             Assert.NotNull(api);
             Assert.NotNull(pars);
@@ -97,7 +48,7 @@ namespace WeatherApp.Tests.Services
         {
             var api = "lat=";
             var pars = $"{lat}&lon={lng}";
-            var data = Setup_WeatherData();
+            var data = new MockWeatherDataSetup().Setup_WeatherData();
             Assert.IsNotNull(data);
             Assert.NotNull(pars);
             Assert.NotNull(api);
